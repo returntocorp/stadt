ba514b50ba9b0bc4f68c98ef552130f8ae0a363f
Ash Zahlen <ash@returntocorp.com>
2019-07-31 16:36:14 -0700

    Add tests for possibleTypes.

--- tests/union.ts
+++ tests/union.ts
@@ -40,3 +40,26 @@ const foo: {a: number} & {b: string} = {a: 3, b: 'hi'};
     assert.equal(ty.toString(), "boolean & Symbol");
   });
 });
+
+describe("possibleTypes", () => {
+  it("returns a list for non-union types", () => {
+    const ty = new adt.LiteralType("foo");
+    assert.sameMembers(ty.possibleTypes(), [ty]);
+  });
+
+  it("returns all the members of a union type", () => {
+    const ty1 = new adt.LiteralType("foo");
+    const ty2 = new adt.LiteralType(1234);
+    const union = new adt.UnionType([ty1, ty2]);
+    assert.sameMembers(union.possibleTypes(), [ty1, ty2]);
+  });
+
+  it("recurses into unions whose members are unions", () => {
+    const ty1 = new adt.LiteralType("foo");
+    const ty2 = new adt.LiteralType(1234);
+    const innerUnion = new adt.UnionType([ty1, ty2]);
+    const ty3 = adt.booleanType;
+    const union = new adt.UnionType([innerUnion, ty3]);
+    assert.sameMembers(union.possibleTypes(), [ty1, ty2, ty3]);
+  });
+});
